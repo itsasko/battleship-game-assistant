5c12aa2e7ceb1f6570cf2e9326cbc3a5b4782058
Oksana <oksa.osidach@gmail.com>
2024-09-27 10:24:30 +0200

    play field, button click event, drawing

---
+++ Player1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Battleship
+{
+    partial class Player1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControlPlayer1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControlPlayer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlPlayer1
+            // 
+            this.tabControlPlayer1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControlPlayer1.Controls.Add(this.tabPage1);
+            this.tabControlPlayer1.Controls.Add(this.tabPage2);
+            this.tabControlPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.tabControlPlayer1.Multiline = true;
+            this.tabControlPlayer1.Name = "tabControlPlayer1";
+            this.tabControlPlayer1.SelectedIndex = 0;
+            this.tabControlPlayer1.Size = new System.Drawing.Size(927, 908);
+            this.tabControlPlayer1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(23, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(900, 900);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "You";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(23, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(900, 900);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Opponent";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // Player1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 908);
+            this.Controls.Add(this.tabControlPlayer1);
+            this.Name = "Player1";
+            this.Text = "Player1";
+            this.tabControlPlayer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControlPlayer1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
---
+++ Player1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleship
+{
+    public partial class Player1 : Form
+    {
+        bool isGameStarted = false;
+        private Button[] playerButtons = new Button[100];
+        private Button[] opponentButtons = new Button[100];
+        private char[] columnLabels = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+        public Player1()
+        {
+            InitializeComponent();
+            CreatePlayerButtons();
+            CreateOpponentButtons();
+            MessageBox.Show("Locate carrier (occupies 5 spaces)");
+            MessageBox.Show("Locate battleship (occupies 4 spaces)");
+            MessageBox.Show("Locate cruiser (occupies 3 spaces)");
+            MessageBox.Show("Locate submarine (occupies 3 spaces)");
+            MessageBox.Show("Locate destroyer (occupies 2 spaces)");
+
+        }
+        private void CreatePlayerButtons()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                int row = i / 10 + 1;
+                char column = columnLabels[i % 10];
+                this.playerButtons[i] = new Button();
+                this.playerButtons[i].Location = new Point((i % 10) * 90, (i / 10) * 90);
+                this.playerButtons[i].Size = new Size(90, 90);
+                this.playerButtons[i].TabIndex = i + 10;
+                this.playerButtons[i].Text = $"{row}{column}";
+                this.playerButtons[i].UseVisualStyleBackColor = true;
+                this.playerButtons[i].BackColor = Color.White;
+                this.playerButtons[i].Click += new System.EventHandler(this.PlayerButtonClick);
+                this.tabPage1.Controls.Add(this.playerButtons[i]); 
+            }
+        }
+
+        private void CreateOpponentButtons()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                int row = i / 10 + 1;
+                char column = columnLabels[i % 10];
+                this.opponentButtons[i] = new Button();
+                this.opponentButtons[i].Location = new Point((i % 10) * 90, (i / 10) * 90);
+                this.opponentButtons[i].Size = new Size(90, 90);
+                this.opponentButtons[i].TabIndex = i + 110;
+                this.opponentButtons[i].Text = $"{row}{column}";
+                this.opponentButtons[i].UseVisualStyleBackColor = true;
+                this.playerButtons[i].BackColor = Color.White;
+                this.playerButtons[i].Click += new System.EventHandler(this.OpponentButtonClick);
+                this.tabPage2.Controls.Add(this.opponentButtons[i]);
+            }
+        }
+        private void PlayerButtonClick(object sender, EventArgs e)
+        {
+            if (!isGameStarted)
+            {
+                Button clickedButton = sender as Button;
+                if (clickedButton.BackColor == Color.White)
+                {
+                    clickedButton.BackColor = Color.Blue;
+                }
+                else
+                {
+                    clickedButton.BackColor = Color.White;
+                }
+            }
+        }
+        private void OpponentButtonClick(object sender, EventArgs e)
+        {
+            if (isGameStarted)
+            {
+                Button clickedButton = sender as Button;
+                if (clickedButton.BackColor == Color.White)
+                {
+                    clickedButton.BackColor = Color.Red;
+                }
+                else
+                {
+                    clickedButton.BackColor = Color.White;
+                }
+            }
+            
+        }
+        private void tabPage1_Click(object sender, EventArgs e) { }
+        private void tabPage2_Click(object sender, EventArgs e) { }
+    }
+}
+
